d1349ccd8c5a955083f151c1cede3fd8ffe8fd49
AshenCreations <112626864+AshenCreations@users.noreply.github.com>
2022-12-05 11:50:03 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	init();
 	atexit(cleanup);
 
-	timeBeginPeriod(1);		// set system sleep granularity to 1ms
+	timeBeginPeriod(1);							// set system sleep granularity to 1ms
 	LARGE_INTEGER previous, current;
 	f64 elapsed, lag = 0.0, totalLag = 0.0;
 	QueryPerformanceCounter(&previous);
@@ -16,12 +16,16 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Gameloop START ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 	// while loop just calls in order input, update, render. Uses QueryPerformanceCounter to time the loop in microseconds,
 	// which we convert to milliseconds when we want to use it. It has some printf logging for timing visualisation.
+	
+	// As the amount of work done & thus the time taken grows in update(), the value of MS_PER_UPDATE will need to be tweaked.
+	// At all times MS_PER_UPDATE must be greater than the time spent in update(), on the slowest machine you are targetting
+
 	while (1)
 	{
 		QueryPerformanceCounter(&current);		// in microseconds
 		elapsed = (f64)(current.QuadPart - previous.QuadPart);
 		previous = current;
-		lag += elapsed / 1000.0;	// divide by 1000 to convert to ms
+		lag += elapsed / 1000.0;		// divide by 1000 to convert to ms
 
 		input();
 
@@ -29,17 +33,17 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		// so that it may be normalised later when divided by MS_PER_UPDATE
 		printf("lag before update %.4f\n", lag);
 
-		int count = 0;						// for counting the number of updates within each iteration of the gameloop
-		while(lag >= MS_PER_UPDATE)			// loop until lag < MS_PER_UPDATE
+		int count = 0;					// for counting the number of updates within each iteration of the gameloop
+		while(lag >= MS_PER_UPDATE)		// loop until lag < MS_PER_UPDATE
 		{
 			update();
 			lag -= MS_PER_UPDATE;
 			count++;
 			printf("count is %d, lag = %.4f\n",count, lag);	// printed to visualise the interative value reduction
 		}
-
-		lag /= MS_PER_UPDATE;						// here lag is less then MS_PER_UPDATE
-													//so we can divide by MS_PER_UPDATE to normalize (0 to 1)
+		
+		// here lag is less then MS_PER_UPDATE, so we can divide by MS_PER_UPDATE to normalize (0 to 1)
+		lag /= MS_PER_UPDATE;
 		printf("normalised lag is %.4f\n", lag);
 		render(lag);
 		printf("Frame Rendered\n");		// printed so we can see how many updateframes there are between renderframes
